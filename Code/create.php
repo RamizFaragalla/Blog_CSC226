1aae8fdf83d75f92d696774bf14aa3938637101d
Michael Militar <mikeymilitar10@gmail.com>
2020-05-06 22:22:00 -0400

    Update create.php

--- Code/create.php
+++ Code/create.php
@@ -1,35 +1,30 @@
-<!-- <?php
+<!--  <?php
 	include "includes/dbconnect.inc.php";
 	session_start();
 ?>
+
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Edit/Delete Blog</title>
+		<title>Create blog</title>
 		<style>
 			.error{
 				color: red;
 			}
-
 			button{
 				width: 100px;
 			}
-
 			* {
       			font-family: consolas;
       			font-size: 15px;
     		}
-
     		textarea {
     			width: 100%;
 				vertical-align: top;
 			}
-
 		</style>
 	</head>
-
 	<body>
-
 
 		<?php
 			if(isset($_GET['error'])){
@@ -37,10 +32,8 @@
 					echo '<p class="error">';
 					echo 'Please fill in all fields';
 					echo '</p>';
-
 				}
 			}
-
 			if(isset($_GET["postID"])) {
 				// query
 				// need: title, content
@@ -50,49 +43,43 @@
 				$stmt->bind_param("i", $_GET["postID"]);
 				$stmt->execute();
 				$result = $stmt->get_result()->fetch_assoc();
-
 				$_SESSION["postID"] = $_GET["postID"];
 			}
 		?>
-
 		
-		<form action="includes/processEdit.inc.php" method="post">
-
+		<form action="createProcess.php" method="post">
 			<p align = "left">
 				<label>Blog Title: <input type="text" name="title" size="20" maxlength="40" placeholder="Title"
-					value="<?php 
+					value="
+					<?php 
 					
 					if(isset($_GET["postID"])) 
 						echo $result["TITLE"];
-
 					else if(isset($_GET['title']))
 						echo $_GET['title']; 
-
 					else echo "";
 					?>">
 				</label>
 			</p>
 
 			<p align = "left">
-				<label >Content: <textarea name="content" rows="15" placeholder="Content"><?php
+				<label >Content: <textarea name="content" rows="15" placeholder="Content">
+					<?php
+					
 					if(isset($_GET["postID"])) 
 						echo $result["CONTENT"];
-
 					else if(isset($_SESSION['content']))
 						echo $_SESSION['content'];
-
-					
+			
 					else echo "";
-					?></textarea>
+					?>
+					</textarea>
 				</label>
 			</p>
 	
 			<p align="center">
-				<input type="submit" name="update" value="Update">
-
-				<input type="submit" name="delete" value="Delete">
+				<input type="submit" name="create" value="Create">
 			</p>
-
 		</form>
 	</body>	
 </html>
